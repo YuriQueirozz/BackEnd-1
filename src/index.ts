03766cbf8be3e61f379fb50e86b720c1781f535c
Yuri <yuque7@gmail.com>
2025-09-27 15:47:47 -0300

    Correção do app.listen em arquivos errados e resolução de bugs ao executar o servidor na porta 3003

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ import { app } from "./app";
 import { userRouter } from "./routes/userRouter";
 
 app.use('/users', userRouter)
+
+app.listen(3003, () => {
+	console.log('Servidor rodando na porta 3003')
+})
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,3 +1,23 @@
 import { Router } from "express";
 
 export const userRouter = Router();
+
+// Teste
+const users = [
+    { id: 1, nome: "Flávio", idade: 18 }
+];
+
+// Rota GET por id
+userRouter.get("/:id", (req, res) => {
+    const userId = parseInt(req.params.id);
+    const user = users.find(u => u.id === userId);
+
+    if (user) {
+        return res.status(404).json({
+            sucess:false,
+            message: "Usuário não encontrado"
+        });
+    };
+
+    res.json(user);
+});
