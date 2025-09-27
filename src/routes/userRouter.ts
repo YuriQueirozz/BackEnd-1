6a2f9e91a633860d6a45a97d71aa274b5d1a4f38
Yuri <yuque7@gmail.com>
2025-09-27 16:23:46 -0300

    Exercício 2 funcionando conforme as variáveis pedidas no guia

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -4,22 +4,24 @@ export const userRouter = Router();
 
 // Teste
 const users = [
-    { id: 1, nome: "Flávio", idade: 18 }
+    { id: 1, nome: "Flávio", idade: 18 },
+    { id: 2, nome: "Nilton", idade: 22 },
+    { id: 3, nome: "Lucas", idade: 25 },
 ];
 
 userRouter.get("/age-range", (req, res) => {
-    const idadeMin = parseInt(req.query.idadeMin as string);
-    const idadeMax = parseInt(req.query.idadeMax as string);
+    const min = parseInt(req.query.min as string);
+    const max = parseInt(req.query.max as string);
 
-    if (isNaN(idadeMin) || isNaN(idadeMax)) {
-        return res.status(400).json({
+    if (isNaN(min) || isNaN(max)) {
+        return res.status(404).json({
             sucess:  false,
-            message: "idadeMin e idadeMax devem ser números"
+            message: "min e max devem ser números"
         });
     }
 
     // Filtrar pela idade
-    const filtragemUsuarios = users.filter(u => u.idade >= idadeMin && u.idade <= idadeMax);
+    const filtragemUsuarios = users.filter(u => u.idade >= min && u.idade <= max);
 
     res.json(filtragemUsuarios);
 });
